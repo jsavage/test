21b463b3289895730ebdd52fc2a5facc6918aa9a
James Savage <consult.js@gmail.com>
2012-06-21 09:10:04 +0200

    Further refinements

--- test.sh
+++ test.sh
@@ -1,34 +1,45 @@
+#!/bin/bash
+
 #Tiddlyspace Install for Ubuntu 11.04
-#updated with all patches
-#Python 2.7
-#GCC
+#Assumptions:
+#-Basic server build
+#-Networking enabled and working (proxy's configured)
+#-Aim to be able to git clone script and then execute it with putty
+#-OS already updated with all patches
+#-Python 2.7 already present
+#-GCC present
 #------------
-#While Logged in a jsavage - user with admin rights
+#execute sript While logged in as root or a user with admin rights
 
-#Followed C Dents instructions for tiddlyspace
+#Based on C Dents instructions for tiddlyspace
 
-#>Tiddlyweb Instructons
-#>Installing on Ubuntu
+#Refer to C Dents Tiddlyweb Instructons
+#Following section on Installing on Ubuntu
 sudo apt-get install python-pip python-dev
 sudo pip install -U pip
 sudo pip install -U tiddlywebwiki 
-#>back to C Dents Instructions for tiddlyspace
+#back to C Dents Instructions for tiddlyspace
 sudo apt-get install libmysqlclient-dev
-# install mysql-server
+
+#install mysql-server
 sudo apt-get install mysql-server
 
 sudo pip install -U tiddlywebplugins.tiddlyspace
 # The above wasn't sudo in instructions
 
+#Configure a tiddlyspace database
 mysql -u root -p
 create database tiddlyspace
-GRANT ALL PRIVILEGES ON tiddlyspace.* TO '<tiddlydbuser>'@'localhost' IDENTIFIED BY '<dbpassword>';
-quit
-# Am still puzzled by the above - do I need an account called tiddlydbuser?  Does the password for that
-# account need to go here or elsewhere?
+GRANT ALL PRIVILEGES ON tiddlyspace.* TO '<tiddlydbuser>'@'localhost'; \g 
+# IDENTIFIED BY '<dbpassword>';
+quit \g
+# Am still puzzled by the above - do I need an account called tiddlydbuser?  
+# Does the password for that account need to go here or elsewhere?
+# If the database is called something else what else must we change?
 
 # The above database config exactly as per C Dent intructions
 # skip db_config.py
 
 tiddlyspace tiddlyspace_instance  # Errors received at this point
 #  "Access denied for user 'jsavage'@'localhost' (using password: NO)") None None
+# Question - what if we want to repeat the tiddlyspace command?  Is there an option to over-write existing stuff? 
